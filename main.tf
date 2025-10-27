893c62fbdc775a7f8c685f90cc438e9e153e111d
Naresh Doosa <nareshdoosa11@gmail.com>
2025-10-27 19:21:46 +0530

    Added VPC and subnet references for EC2

--- main.tf
+++ main.tf
@@ -2,28 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 🔍 Get the first available VPC
+# Get default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-# 🔍 Get a subnet in that VPC
+# Get a default subnet
 data "aws_subnet" "default" {
   default_for_az = true
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0360c520857e3138f"
-  instance_type = "t2.micro"
-  subnet_id     = data.aws_subnet.default.id    # 👈 use subnet explicitly
-  vpc_security_group_ids = [aws_security_group.sg.id] # 👈 reference your SG (if any)
-  tags = {
-    Name = "Jenkins-Terraform-EC2"
-  }
-}
-
-resource "aws_security_group" "sg" {
-  name        = "jenkins-tf-sg"
+# Security group inside that VPC
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
   description = "Allow SSH and HTTP"
   vpc_id      = data.aws_vpc.default.id
 
@@ -34,10 +25,29 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0360c520857e3138f"
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet.default.id
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
+  tags = {
+    Name = "Jenkins-Terraform-EC2"
   }
 }
